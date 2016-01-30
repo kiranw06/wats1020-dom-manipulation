3d98e110d4e7d062b0a0512ef929253d90efb7e0
kiranw6691 <kiranw6691@gmail.com>
2016-01-30 16:00:41 -0500

    added view details function

--- js/main.js
+++ js/main.js
@@ -28,7 +28,21 @@ $( document ).ready(function() {
     // buttons so that when a user clicks a "View Details" button they see
     // the content contained in the elements with the class "details" in the
     // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
+    $ ('.view-details').on('click' , function(event) {
+      console.log(event);
+      var targetElement = event.target;
+      //      1. When user clicks a "view details" button, find the parent of that element.
+      var container = targetElement.parentElement.parentElement; //TODO use multiplier 
+      $ (container).find('.details').each( function(index, el) {
+          if ($(el).is(':visible')) {
+            $(el).fadeOut();
+            targetElement.innerHTML = "View Details"
+          } else {
+            $(el).fadeIn();
+            targetElement.innerHTML = "Hide Details"
+          }
+      });
+     });
     //      2. Within that parent, find all the elements that have the class `details`.
     //      3. Toggle visibility of all the elements within that parent with the class `details`.
     //      4. Change the text of the "view details" button to read "hide details" so the user
